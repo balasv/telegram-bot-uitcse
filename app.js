0811fa281a1a162ac55d0897e6323257fd12d694
balasv <bala.k.btech@gmail.com>
2016-02-06 01:52:42 -0800

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ console.log('Express server listening on port ' + app.get('port'));
     
 var unirest = require('unirest');
 var stringTable = require('string-table');
-var BASE_URL = "https://api.telegram.org/bot185602892:AAGXEyMR9_UteyIvobwOWywTT9FdZxCn8C0/";
+var BASE_URL = "https://api.telegram.org/bot89371812:AAGawhl9qP5nOO9B6OcI-9_l9mYxj4WZfCM/";
 var POLLING_URL = BASE_URL + "getUpdates?offset=:offset:&timeout=60";
 var SEND_MESSAGE_URL = BASE_URL + "sendMessage";
 var max_offset = 0;
